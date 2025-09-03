ffc9222fc966986d8549d361a04d17c4a65c5924
edsondba <edsonjlbh4@gmail.com>
2025-09-03 17:27:04 -0300

    Update ErrorLog procurar falhas de logon.sql

--- Login/ErrorLog procurar falhas de logon.sql
+++ Login/ErrorLog procurar falhas de logon.sql
@@ -8,3 +8,18 @@ EXEC master.dbo.xp_readerrorlog 0, 1, N'Login failed'
 
 -- COMO IDENTIFICAR FALHAS DE LOGIN POR SENHA INCORRETA
 EXEC master.dbo.xp_readerrorlog 0, 1, N'Login failed', N'password'
+
+-- IDENTIFICANDO USUÁRIO E MÁQUINA
+IF (OBJECT_ID('tempdb..#Login_Failed') IS NOT NULL) DROP TABLE #Login_Failed
+CREATE TABLE #Login_Failed ( 
+    [LogDate] DATETIME, 
+    [ProcessInfo] NVARCHAR(50) COLLATE SQL_Latin1_General_CP1_CI_AI, 
+    [Text] NVARCHAR(MAX) COLLATE SQL_Latin1_General_CP1_CI_AI,
+    [Username] AS LTRIM(RTRIM(REPLACE(REPLACE(SUBSTRING(REPLACE([Text], 'Login failed for user ''', ''), 1, CHARINDEX('. Reason:', REPLACE([Text], 'Login failed for user ''', '')) - 2), CHAR(10), ''), CHAR(13), ''))),
+    [IP] AS LTRIM(RTRIM(REPLACE(REPLACE(REPLACE((SUBSTRING([Text], CHARINDEX('[CLIENT: ', [Text]) + 9, LEN([Text]))), ']', ''), CHAR(10), ''), CHAR(13), '')))
+)
+
+INSERT INTO #Login_Failed (LogDate, ProcessInfo, [Text]) 
+EXEC master.dbo.xp_readerrorlog 0, 1, N'Login failed'
+
+SELECT * FROM #Login_Failed --where [Username] = 'NOME_DO_LOGIN' AND [LogDate] LIKE '%30%' ORDER BY [LogDate] DESC
