0ae61a2afdb2c36bb3d4e3ba306b57a648f72a36
edsondba <edsonjlbh4@gmail.com>
2025-09-04 19:25:09 -0300

    Update Statistics verificar o status atual.sql

--- Performance SQL Server/Statistics verificar o status atual.sql
+++ Performance SQL Server/Statistics verificar o status atual.sql
@@ -23,3 +23,26 @@ CROSS APPLY sys.dm_db_stats_properties(stat.object_id, stat.stats_id) AS sp
 WHERE       obj.is_ms_shipped = 0
 AND (rows_sampled* 100 / [rows]) < 70
 ORDER BY    modification_counter DESC
+
+
+-- VERIFICAÇÃO ESTATÍSTICAS (GERAL)
+SELECT OBJECT_SCHEMA_NAME(obj.object_id) SchemaName, obj.name TableName, 
+ stat.name, modification_counter, 
+--modification_counter * 100 / [rows] as [% modification_counter],
+[rows],
+rows_sampled, rows_sampled* 100 / [rows] AS [% Rows Sampled],
+last_updated
+FROM sys.objects AS obj
+INNER JOIN sys.stats AS stat ON stat.object_id = obj.object_id
+CROSS APPLY sys.dm_db_stats_properties(stat.object_id, stat.stats_id) AS sp
+WHERE  obj.is_ms_shipped = 0
+and rows is not null
+--and rows_sampled* 100 / [rows] < 80
+--and last_updated <= dateadd(day,-7,getdate())
+--and modification_counter * 100 / [rows] > 20
+and obj.name not like ('%_TTAT_LOG%')
+--and OBJECT_SCHEMA_NAME(obj.object_id) != 'TOTVSAUDIT'
+--and  obj.name in ('')
+--ORDER BY modification_counter DESC
+--ORDER BY rows desc
+ORDER BY modification_counter desc, last_updated DESC
